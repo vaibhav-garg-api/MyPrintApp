b150802becf81bb4222df8742e87243e9f688212
vaibhav-garg-api <73463322+vaibhav-garg-api@users.noreply.github.com>
2020-10-30 15:06:57 +0530

    Add files via upload

---
+++ AllPermutations.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class AllPermutations {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		System.out.println(permutate(s));
+	}
+	public static ArrayList<String> permutate(String s){
+		if(s.equals("")){
+			ArrayList<String> baseList = new ArrayList<>();
+			baseList.add("");
+			return baseList;
+		}
+		
+		
+		char c = s.charAt(0);
+		String rs = s.substring(1);
+		ArrayList<String> MyResult = new ArrayList<>();
+		ArrayList<String> RemResult = permutate(rs);
+		for(String ps:RemResult){
+			for(int i=0;i<=ps.length();i++){
+				String val= ps.substring(0,i)+c+ps.substring(i);
+				MyResult.add(val);
+			}	
+		}
+		return MyResult;
+	}
+			
+}
